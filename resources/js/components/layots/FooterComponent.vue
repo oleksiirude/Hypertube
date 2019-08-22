a4443fb987798fc55a4fa5b093fc5db67386dd91
dasha.cherednichenko <dasha.cherednichenko@gmail.com>
2019-08-22 20:11:20 +0300

    language change

--- resources/js/components/layots/FooterComponent.vue
+++ resources/js/components/layots/FooterComponent.vue
@@ -1,14 +1,36 @@
 <template>
     <footer>
         <div class="upper_footer"></div>
-        <p class="copyright">
-            &copy; created by US, 2019
-        </p>
+        <div class="footer_content">
+            <p class="copyright">
+                &copy; created by US, 2019
+            </p>
+            <div class="top-right links">
+                <div class="language">
+                    <a :href="en_url">
+                        <img :src="en_img" id="en" alt="English">
+                    </a>
+                    <a :href="ua_url">
+                        <img :src="ua_img" id="ua" alt="Ukrainian">
+                    </a>
+                </div>
+            </div>
+        </div>
     </footer>
 </template>
 
 <script>
     export default {
+        mounted(){
+            let lang = document.documentElement.lang;
+            if (lang === 'ua') {
+                document.getElementById('ua').classList.add("active_lng");
+            }
+            else {
+                document.getElementById('en').classList.add("active_lng");
+            }
+            console.log('lang', document.documentElement.lang);
+        },
         name: "FooterComponent.vue",
         props: [
             'ua_url',
@@ -28,7 +50,11 @@
 
         text-align: right;
     }
+    .footer_content {
+        text-align: left;
+    }
     .copyright {
+        display: inline-block;
         text-align: left;
         padding-left: 20px;
         /*padding-top: 10px;*/
@@ -58,5 +84,22 @@
         z-index: -1;
 
     }
+    .top-right {
+        display: inline-block;
+        float: right;
+    }
+    .language {
+        margin-right: 20px;
+    }
+    .language > a > img {
+        opacity: 0.4;
+    }
+    .language > a > img:hover {
+        opacity: 1.0;
+    }
+    .language > a > .active_lng {
+        opacity: 1.0;
+    }
+
 
 </style>

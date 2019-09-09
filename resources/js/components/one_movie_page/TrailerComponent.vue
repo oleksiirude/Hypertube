61d30c9ffc3d5b18e25599660415e6d4c64bebe6
dasha.cherednichenko <dasha.cherednichenko@gmail.com>
2019-09-09 20:06:58 +0300

    trailer watching done, but not adaptive design

--- resources/js/components/one_movie_page/TrailerComponent.vue
+++ resources/js/components/one_movie_page/TrailerComponent.vue
@@ -1,13 +1,58 @@
 <template>
-    
+    <div class="trailer_div">
+        <a class="watch-btn" :href="trailer" target="_blank">
+            <div class="icon trailer"></div>
+            <div class="caption" >trailer</div>
+        </a>
+    </div>
 </template>
 
 <script>
     export default {
-
+        props: [
+            'trailer'
+        ],
     }
 </script>
 
 <style scoped>
+    .trailer_div {
+        margin-top: 20px;
+        margin-bottom: 40px;
+    }
+    .watch-btn {
+        display: inline-block;
+        text-align: center;
+        width: 150px;
+        height: 60px;
+        position: relative;
+        color: #fff;
+        /* -webkit-animation: neon2 1.5s ease-in-out infinite alternate; */
 
+        /*height: 100%;*/
+        border-radius: 4px;
+        border: 1px rgba(255,255,255, 0.91) solid;
+        cursor: pointer !important;
+        box-shadow: 2px 2px 2px rgba(0, 0, 0, 1);
+    }
+    .watch-btn:hover {
+        text-decoration: none;
+        text-shadow: 0 0 5px #228DFF, 0 0 10px #228DFF, 0 0 15px #228DFF, 0 0 20px #fff, 0 0 35px #fff, 0 0 40px #228DFF, 0 0 50px #228DFF, 0 0 75px #228DFF;
+    }
+    .watch-btn .icon.trailer {
+        display: inline-block;
+        /*position: absolute;*/
+        font-size: 32px;
+        /*left: 25px;*/
+        /*top: 0;*/
+        line-height: 60px;
+    }
+    .icon.trailer:before {
+        content: '\f144';
+        font-family: FontAwesome;
+    }
+    .caption {
+        display: inline-block;
+        text-transform: uppercase;
+    }
 </style>
--- resources/views/watch.blade.php
+++ resources/views/watch.blade.php
@@ -27,12 +27,8 @@
                             {{  $content->summary }}
                         </div>
 
-{{--                        <div class="sep">--}}
-{{--                            <div class="watch-btn" onclick="">--}}
-{{--                                <div class="icon2 trailer"></div>--}}
-{{--                                <div class="caption" >trailer</div>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
+                        <trailer-component trailer="http://www.youtube.com/embed/{{ $content->yt_trailer_code }}"></trailer-component>
+
 {{--                        <div class="embed-responsive embed-responsive-16by9">--}}
 {{--                            <iframe class="embed-responsive-item" src="http://www.youtube.com/embed/{{ $content->yt_trailer_code }}" allowfullscreen style="width: 500px; height: 300px"></iframe>--}}
 {{--                        </div>--}}
